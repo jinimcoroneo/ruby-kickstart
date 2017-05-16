b7802ba251ac418621128c3e6ac1f5908b65e246
jinimcoroneo <jinim1983@gmail.com>
2017-05-16 22:37:32 +0100

    finished session 1 challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,16 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  array = string.split("")
+  array_2 = []
+  array.each_with_index do |letter, index|
+    if letter == "r" || letter == "R"
+      array_2 << array[index + 1]
+    end
+  end
+  return array_2.join
 end
