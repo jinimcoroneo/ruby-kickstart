e063f7ac492a815c822ac235e1fd29e44464ecef
jinimcoroneo <jinim1983@gmail.com>
2017-05-23 23:53:16 +0100

    Session 3 challenge 4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,9 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+	frequencies = Hash.new
+	str.split(" ").each_with_index { |word,index| frequencies[word] ||= index }
+	frequencies
+end
