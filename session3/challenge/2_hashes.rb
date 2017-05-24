8a134a142d717c63b0462ebde02c7ec3a891866d
jinimcoroneo <jinim1983@gmail.com>
2017-05-24 16:23:48 +0100

    Session 3 challenge 2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -1,5 +1,7 @@
-# Given a nonnegative integer, return a hash whose keys are all the odd nonnegative integers up to it
-# and each key's value is an array containing all the even non negative integers up to it.
+# Given a nonnegative integer, return a hash whose keys are all the odd 
+#nonnegative integers up to it
+# and each key's value is an array containing all the even non negative 
+#integers up to it.
 #
 # Examples:
 # staircase 1  # => {1 => []}
@@ -7,4 +9,15 @@
 # staircase 3  # => {1 => [], 3 => [2]}
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
+
+def staircase(n)
+  hash = Hash.new
+  (0..n).to_a.select do |num| 
+  	if num % 2 == 1 
+  	  hash[num] = (1..num).to_a.select {|num| num % 2 == 0}
+  	end
+  end
+  hash
+end
+
 
